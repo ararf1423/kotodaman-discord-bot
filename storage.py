45b7a3a6082d12d48a5a51858a3b16901a7a3f26
ararf1423 <5n5zpmb2z9@privaterelay.appleid.com>
2026-07-19 16:38:17 +0900

    Create storage.py

---
+++ storage.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+FILE = "events.json"
+
+
+def load():
+
+    if not os.path.exists(FILE):
+        return []
+
+    with open(FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def save(data):
+
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(
+            data,
+            f,
+            ensure_ascii=False,
+            indent=4
+        )
