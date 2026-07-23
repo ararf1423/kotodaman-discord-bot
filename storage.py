d6654f2d04e874c530ecb4ddc2535f221a678fd5
ararf1423 <5n5zpmb2z9@privaterelay.appleid.com>
2026-07-23 10:16:38 +0900

    storage.py を更新

--- storage.py
+++ storage.py
@@ -1,24 +1,17 @@
 import json
 import os
 
-FILE = "events.json"
+FILE_NAME = "events.json"
 
 
-def load():
-
-    if not os.path.exists(FILE):
+def load_events():
+    if not os.path.exists(FILE_NAME):
         return []
 
-    with open(FILE, "r", encoding="utf-8") as f:
+    with open(FILE_NAME, "r", encoding="utf-8") as f:
         return json.load(f)
 
 
-def save(data):
-
-    with open(FILE, "w", encoding="utf-8") as f:
-        json.dump(
-            data,
-            f,
-            ensure_ascii=False,
-            indent=4
-        )
+def save_events(events):
+    with open(FILE_NAME, "w", encoding="utf-8") as f:
+        json.dump(events, f, ensure_ascii=False, indent=4)
